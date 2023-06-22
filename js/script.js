1e33dae08efdb549a6bb0a23d4fe7202421f3610
lipamaoa <filipa.ferreirasilva@gmail.com>
2023-06-23 00:09:42 +0100

    done without the bonus

--- js/script.js
+++ js/script.js
@@ -1,12 +1,75 @@
-window.onload = function () {
+window.addEventListener('load', () => {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
 
-  startButton.addEventListener("click", function () {
-    startGame();
-  });
+  let game = null;
 
   function startGame() {
     console.log("start game");
+    game = new Game();
+    game.start();
   }
-};
+
+  document.addEventListener('keydown', event => {
+    const key = event.key;
+    const possibleKeystrokes = [
+      'ArrowLeft',
+      'ArrowUp',
+      'ArrowRight',
+      'ArrowDown',
+    ];
+
+    if (possibleKeystrokes.includes(key)) {
+
+      event.preventDefault();
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+
+    }
+  });
+
+  document.addEventListener('keyup', event => {
+    const key = event.key;
+    const possibleKeystrokes = [
+      'ArrowLeft',
+      'ArrowUp',
+      'ArrowRight',
+      'ArrowDown',
+    ];
+
+    if (possibleKeystrokes.includes(key)) {
+
+      event.preventDefault();
+      switch (key) {
+        case "ArrowLeft":
+        case "ArrowRight":
+          game.player.directionX = 0;
+          break;
+        case "ArrowUp":
+        case "ArrowDown":
+          game.player.directionY = 0;
+          break;
+      }
+
+    }
+    })
+  startButton.addEventListener("click", function () {
+    startGame();
+  });
+  restartButton.addEventListener("click", function () {
+    startGame();
+    //location.reload()
+  });
+});
